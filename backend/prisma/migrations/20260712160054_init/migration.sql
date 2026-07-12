79ee60b8062fcc87b321afeeae038181e39fb393
Nuthan K <nuthank0435@gmail.com>
2026-07-12 21:31:59 +0530

    feat: configure PostgreSQL and Prisma schema

---
+++ backend/prisma/migrations/20260712160054_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateEnum
+CREATE TYPE "LinkPrecedence" AS ENUM ('primary', 'secondary');
+
+-- CreateTable
+CREATE TABLE "contacts" (
+    "id" SERIAL NOT NULL,
+    "phoneNumber" TEXT,
+    "email" TEXT,
+    "linkedId" INTEGER,
+    "linkPrecedence" "LinkPrecedence" NOT NULL DEFAULT 'primary',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "contacts_email_idx" ON "contacts"("email");
+
+-- CreateIndex
+CREATE INDEX "contacts_phoneNumber_idx" ON "contacts"("phoneNumber");
+
+-- CreateIndex
+CREATE INDEX "contacts_linkedId_idx" ON "contacts"("linkedId");
